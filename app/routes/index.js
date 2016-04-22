f9fe7467fd65f2ebe1eff95f3fe7af60b9b6ce30
Jacob Oscarson <jacob@plexical.com>
2016-04-22 12:53:20 +0200

    Ops! Forgot to add the new `/tests` route to routes!

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,10 +6,13 @@ import Start from '../screens/start';
 import About from '../screens/about';
 import Contact from '../screens/contact';
 
+import Tests from '../screens/tests'
+
 export default () => (
   <Route component={ App }>
     <IndexRoute component={ Start } />
     <Route component={ About } path="/about/" />
     <Route component={ Contact } path="/contact/" />
+    <Route component={ Tests } path="/tests/" />
   </Route>
 );
