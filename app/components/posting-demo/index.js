42d13c2662c202247b990c2d4e4de0531b001167
Jacob Oscarson <jacob@plexical.com>
2016-05-03 15:23:40 +0200

    Implemented the sugar-fetch ad-hoc simple GET test in fetch demo form

--- app/components/posting-demo/index.js
+++ app/components/posting-demo/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 import * as sfApi from '../../lib/sugar-fetch';
@@ -5,28 +6,67 @@ import * as sfApi from '../../lib/sugar-fetch';
 import {FlatButton,
         Card,
         CardHeader,
+        TextField,
         Button,
+        CircularProgress,
         Checkbox} from 'material-ui';
 
 export default class Poster extends React.Component {
 
-  shouldFail() {
-    return this.refs.shouldFail.isChecked();;
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    };
+  }
+
+  getUri() {
+    const path = this.refs.sendUri.getValue();
+    return path[0] == '/' && path || '/'+path;
+  }
+
+  loading(state) {
+    this.setState ({loading: state})
   }
 
   doPost() {
-    sfApi.post('http://httpbin.org/post', {foo: 'bar'})
-         .then ((data) => {
-           console.log('JSON echo', data.json);
-         })
+    this.loading(true);
+    let self = this;
+    (sfApi.post(`http://httpbin.org${this.getUri()}`, {foo: 'bar'})
+          .then ((data) => {
+            console.log('JSON echo', data.json);
+            self.loading(false);
+          }))
+  }
+
+  doGet() {
+    let self = this;
+    (sfApi.get(`http://httpbin.org${this.getUri()}`, {foo: 'bar'})
+          .then ((data) => {
+            self.loading(false);
+          }))
+    this.loading(true);
   }
 
   render() {
+    let loadingState;
+    if(this.state.loading) {
+      loadingState = <CircularProgress
+                      mode="indeterminate" color="red" size={0.4}/>
+
+    } else {
+      loadingState = <span style={{color: 'green', marginRight: '4px'}}>
+        DONE
+      </span>
+    }
     return (
       <Card>
-      <CardHeader title="API Calling"/>
-      <Checkbox label="Fail" ref="shouldFail"/>
-      <FlatButton onClick={ () => (this.doPost())}>POST</FlatButton>
+        <CardHeader title="API Calling"/>
+        {loadingState}
+        <span>{"http://httpbin.org/ "}</span>
+        <TextField ref="sendUri"/>
+        <FlatButton onClick={ () => (this.doPost())}>POST</FlatButton>
+        <FlatButton onClick={ () => (this.doGet())}>GET</FlatButton>
       </Card>
     );
   }
