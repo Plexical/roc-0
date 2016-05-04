97d29bee8afbf87fef952e16c9a69e4b2409f304
Jacob Oscarson <jacob@plexical.com>
2016-05-04 16:06:41 +0200

    Added quick-n-dirty test grepping to Node.js test runner

--- scripts/browser-free.js
+++ scripts/browser-free.js
@@ -9,7 +9,15 @@ const mocha = new Mocha({
   ui: 'bdd',
   reporter: (new Set(process.argv)).has('--report=min') && 'min' });
 
+const grepWords = (
+  process.argv.slice(-2).filter((s) => (
+    s != 'node' && s.indexOf('browser-free') == -1)) )
+if(grepWords.length > 0) {
+  mocha.grep(grepWords.join(' '))
+}
+
 mocha.addFile('./cli-tests/redux.clicker.test.js');
+mocha.addFile('./cli-tests/redux.network.test.js');
 
 /* Runs & ensures the number of failures is reported as process exit
  value (usable when run non-interactively: $? == 0 -> no failures,
